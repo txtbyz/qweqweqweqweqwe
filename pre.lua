094ff6425c1f4ed32b63c9ef89d177c644390478
txtbyz <78022211+txtbyz@users.noreply.github.com>
2022-09-09 19:29:51 +0300

    Update pre.lua

--- pre.lua
+++ pre.lua
@@ -1,7 +1,7 @@
 local key = _G.Key
 local check = "https://raw.githubusercontent.com/txtbyz/qweqweqweqweqwe/main/check.php?key=" .. key
 if game:HttpGet(check) == "Whitelisted" then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/txtbyz/qweqweqweqweqwe/main/check.php"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/txtbyz/qweqweqweqweqwe/main/script.lua"))()
 else
 game.Players.LocalPlayer:Kick("Premium Kacagi")
 end
